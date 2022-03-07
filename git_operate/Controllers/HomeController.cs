0864e8e6324f9c663397f8e8488d2a1b67430d70
“zliangwei” <2220791137@qq.com>
2022-03-07 22:05:58 +0800

    git操作代码样例

---
+++ git_operate/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using git_operate.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace git_operate.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            
+            return View();
+        }
+
+        public string Demo()
+        {
+            return "想知道git怎么用嘛";
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
